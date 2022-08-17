fef7df39a621aacefbb885f31fdb039ad8a5a0d1
Mikkel Paulick <mikkelpaulick@hotmail.com>
2022-08-17 21:36:07 +0200

    Update main.cpp

--- automation/test/src/main.cpp
+++ automation/test/src/main.cpp
@@ -4,22 +4,21 @@
 #define PINLED 23
 #define BREAKTIME 100
 
-void setup() {
+void setup()
+{
 	// hello world
 	Serial.begin(115200);
 	Serial.println("Hello World!");
 
 	// setup pin
 	pinMode(PINLED, OUTPUT);
-
 }
 
-void loop() {
+void loop()
+{
 	// blink io pin
 	digitalWrite(PINLED, HIGH);
 	delayMicroseconds(BREAKTIME);
 	digitalWrite(PINLED, LOW);
 	delayMicroseconds(BREAKTIME);
-	
-
 }
