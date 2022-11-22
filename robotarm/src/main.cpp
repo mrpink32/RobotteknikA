730caa50e21c68e9617e84cd182e51d97abc0147
mmoltkeleth <mmoltkeleth@gmail.com>
2022-11-22 11:15:05 +0100

    gg
    u9u9evfvgf

--- robotarm/src/main.cpp
+++ robotarm/src/main.cpp
@@ -68,7 +68,7 @@ bool mode_pos = true;
 // Globals
 AsyncWebServer Server(http_port);
 WebSocketsServer WebSocket = WebSocketsServer(ws_port);
-H_Bridge HBridge;
+H_Bridge hBridge;
 char MsgBuf[32];
 int32_t LedState = 0;
 int32_t SliderVal = 0;
@@ -375,6 +375,25 @@ void setup_network()
 
 void motion_task(void *arg)
 {
+  home();
+
+  vTaskDelay(5000);
+  int32_t n = 0;
+  //int32_t steps_pr_deg = 1920;
+
+  //TickType_t xTimeIncrement = configTICK_RATE_HZ/10;
+  //TickType_t xLastWakeTime = xTaskGetTickCount();
+  for (;;)
+    log_v("motion_task...");
+
+    //set_pos(steps_pr_deg*(90+n*90)); // 90 deg
+    set_pos(n * 500 * 4 * 4.8); // 1 round
+    wait_move();
+    vTaskDelay(5000);
+    n++;
+    //vTaskDelayUntil( &xLastWakeTime, xTimeIncrement);
+
+
   HBridge.begin(PIN_MOTOR_CTRL, PIN_MOTOR_INA, PIN_MOTOR_INB,
                 PWM_FREQ_HZ, PWM_RES_BITS, PWM_CH);
 
@@ -471,29 +490,6 @@ void home()
 void set_pos(int32_t pos)
 {
   req_pos = pos;
-}
-
-void motion_task(void *arg)
-{
-  home();
-
-  vTaskDelay(5000);
-  int32_t n = 0;
-  //int32_t steps_pr_deg = 1920;
-
-  //TickType_t xTimeIncrement = configTICK_RATE_HZ/10;
-  //TickType_t xLastWakeTime = xTaskGetTickCount();
-  for (;;)
-  {
-    log_v("motion_task...");
-
-    //set_pos(steps_pr_deg*(90+n*90)); // 90 deg
-    set_pos(n * 500 * 4 * 4.8); // 1 round
-    wait_move();
-    vTaskDelay(5000);
-    n++;
-    //vTaskDelayUntil( &xLastWakeTime, xTimeIncrement);
-  }
 }
 
 void setup()
